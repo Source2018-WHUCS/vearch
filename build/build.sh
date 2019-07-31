d09b6a0bf41f1244bc42627794f24c892b7dfc4e
root <root@A01-R04-I221-17.JD.LOCAL>
2019-07-31 16:53:16 +0800

    add build script

---
+++ build/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ROOT=$(dirname "$PWD")
+BUILDOUT=$ROOT/build/bin/
+mkdir -p $BUILDOUT
+GAMMAOUT=$ROOT/build/gamma_build
+mkdir -p $GAMMAOUT
+
+
+# version value
+BUILD_VERSION="0.1"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT/ps/engine/gammacb/lib/lib/;
+
+flags="-X 'config.BuildVersion=$BUILD_VERSION' -X 'config.CommitID=$(git rev-parse HEAD)' -X 'config.BuildTime=$(date +"%Y-%m-%d %H:%M.%S")'"
+
+echo "version info: $flags"
+
+echo "build gamma"
+
+cd $GAMMAOUT
+cmake -DPERFORMANCE_TESTING=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/ps/engine/gammacb/lib $ROOT/engine/gamma/src/
+make gamma -j  && make install
+cd ../
+
+echo "build baudengine"
+go build -a -tags="vector ump" -ldflags "$flags" -o $BUILDOUT/baudengine $ROOT/startup.go
+
+
+echo "build deploy tool"
+go build -a -ldflags "$flags" -o $BUILDOUT/batch_deployment $ROOT/tools/deployment/batch_deployment.go
