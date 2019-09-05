87f8ec4b519fbd7ef8711bf342abe8edae7deb17
sunjian126 <sunjian11@jd.com>
2019-09-05 22:37:59 +0800

    fix compile path

--- cloud/compile.sh
+++ cloud/compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -v /Users/sunjian/go/src/github.com/vearch/vearch:/vearch vearch_env /vearch/cloud/compile/compile.sh
+docker run -it -v $(dirname "$PWD"):/vearch vearch_env /vearch/cloud/compile/compile.sh
