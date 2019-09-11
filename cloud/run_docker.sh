ab983e61b35f5ef77c0d56086b6dd8d567e377ac
sunjian126 <sunjian11@jd.com>
2019-09-11 13:59:20 +0800

    add get version script

--- cloud/run_docker.sh
+++ cloud/run_docker.sh
@@ -13,3 +13,5 @@ echo "Start service by all in one model"
 docker run -p 8817:8817 -p 9001:9001 -v ../config/config.toml.example.toml:/vearch/config.toml  vearch all
 
 echo "good luck service is ready you can visit http://127.0.0.1:9001 to use it"
+
+curl http://127.0.0.1:9001
