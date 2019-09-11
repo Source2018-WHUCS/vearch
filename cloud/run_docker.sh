b5be00919139a4eafa1d5562b140c3c7ba0f6667
sunjian126 <sunjian11@jd.com>
2019-09-11 14:00:45 +0800

    fix docker name

--- cloud/run_docker.sh
+++ cloud/run_docker.sh
@@ -10,7 +10,7 @@ echo "Make Vearch Image"
 ./build.sh
 
 echo "Start service by all in one model"
-docker run -p 8817:8817 -p 9001:9001 -v ../config/config.toml.example.toml:/vearch/config.toml  vearch all
+docker run -p 8817:8817 -p 9001:9001 -v ./config/config.toml.example:/vearch/config.toml  vearch all
 
 echo "good luck service is ready you can visit http://127.0.0.1:9001 to use it"
 
