61c6846e1313b040bc3cd7066f4c21a31b78d406
wangxingda <wangxingda@jd.com>
2021-03-17 13:46:04 +0800

    remove faiss install

--- cloud/compile/compile.sh
+++ cloud/compile/compile.sh
@@ -2,9 +2,8 @@
 #add env
 export GOROOT=/env/app/go
 export PATH=$PATH:$GOROOT/bin
-export FAISS_HOME=/env/app/faiss_install
 export ROCKSDB_HOME=/env/app/rocksdb_install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/env/app/faiss_install/lib:/env/app/rocksdb_install/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/env/app/rocksdb_install/lib
 # to compile
 cd /vearch/build
 mkdir -p /env/app/go/src/github.com/vearch
@@ -15,7 +14,6 @@ cd /env/app/go/src/github.com/vearch/vearch/build
 
 mkdir -p /vearch/build/lib/
 
-cp /env/app/faiss_install/lib/libfaiss.so /vearch/build/lib/
 cp /env/app/rocksdb_install/lib/librocksdb.* /vearch/build/lib/
 cp /env/app/zfp_install/lib/libzfp.* /vearch/build/lib/
 cp /vearch/build/gamma_build/libgamma.* /vearch/build/lib/
--- cloud/compile_env.sh
+++ cloud/compile_env.sh
@@ -3,4 +3,3 @@
 cd env
 mkdir app
 docker build -t vearch/vearch_env:3.2.6 .
-
