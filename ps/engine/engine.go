d7abccf2beae8824b419bfd0dd1ac26fc24979cb
zhanchao1 <zhanchao3@jd.com>
2023-08-16 17:43:05 +0800

    add max_docid info in cluster/health and cluster/stats

--- ps/engine/engine.go
+++ ps/engine/engine.go
@@ -67,7 +67,7 @@ type Engine interface {
 	NewSnapshot() (proto.Snapshot, error)
 	ApplySnapshot(peers []proto.Peer, iter proto.SnapIterator) error
 	Optimize() error
-	IndexInfo() (int, int)
+	IndexInfo() (int, int, int)
 	EngineStatus(status *EngineStatus) error
 	Close()
 	HasClosed() bool
