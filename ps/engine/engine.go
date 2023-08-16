5922a362e9605980b540ebc685f08e9e0ff706a9
zhanchao1 <zhanchao3@jd.com>
2023-08-16 17:18:20 +0800

    add interface for get doc by docid on specify partition

--- ps/engine/engine.go
+++ ps/engine/engine.go
@@ -26,7 +26,7 @@ import (
 
 // Reader is the read interface to an engine's data.
 type Reader interface {
-	GetDoc(ctx context.Context, doc *vearchpb.Document) error
+	GetDoc(ctx context.Context, doc *vearchpb.Document, getByDocId bool) error
 
 	ReadSN(ctx context.Context) (int64, error)
 
