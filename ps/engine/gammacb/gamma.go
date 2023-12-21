a9720a90ead7c08a1acd7028281108478e6840c7
wangxingda <wangxingda@jd.com>
2023-12-21 15:30:12 +0800

    add partition id to space_name

--- ps/engine/gammacb/gamma.go
+++ ps/engine/gammacb/gamma.go
@@ -24,6 +24,7 @@ import (
 	"time"
 	"unsafe"
 
+	"github.com/spf13/cast"
 	"github.com/vearch/vearch/config"
 	"github.com/vearch/vearch/engine/sdk/go/gamma"
 	"github.com/vearch/vearch/proto/entity"
@@ -62,7 +63,7 @@ func New(cfg register.EngineConfig) (engine.Engine, error) {
 
 	config := &gamma.Config{
 		Path:      cfg.Path,
-		SpaceName: cfg.Space.Name,
+		SpaceName: cfg.Space.Name + "-" + cast.ToString(cfg.PartitionID),
 		LogDir:    config.Conf().GetLogDir()}
 
 	ge := &gammaEngine{
--- ps/engine/gammacb/util.go
+++ ps/engine/gammacb/util.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/spf13/cast"
 	"github.com/vearch/vearch/engine/sdk/go/gamma"
 	"github.com/vearch/vearch/proto/entity"
 	"github.com/vearch/vearch/proto/vearchpb"
@@ -81,7 +82,7 @@ func mapping2Table(cfg register.EngineConfig, m *mapping.IndexMapping) (*gamma.T
 	}
 
 	table := &gamma.Table{
-		Name:            cfg.Space.Name,
+		Name:            cfg.Space.Name + "-" + cast.ToString(cfg.PartitionID),
 		IndexingSize:    int32(engine.IndexSize),
 		RetrievalType:   engine.RetrievalType,
 		RetrievalTypes:  engine.RetrievalTypes,
