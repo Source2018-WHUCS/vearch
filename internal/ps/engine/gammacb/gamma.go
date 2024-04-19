abf9e1362ead4ea4d02a8e353ee6d6e1988dd55e
wangxingda <wangxingda@jd.com>
2024-04-19 14:30:58 +0800

    add engine search and createtable error msg in response

--- internal/ps/engine/gammacb/gamma.go
+++ internal/ps/engine/gammacb/gamma.go
@@ -94,10 +94,10 @@ func New(cfg EngineConfig) (engine.Engine, error) {
 	infos, _ := os.ReadDir(cfg.Path)
 
 	startTime := time.Now()
-	if resp := gamma.CreateTable(ge.gamma, table); resp != 0 {
-		log.Error("create table[%s] for gamma has err [%d] cost time: [%v]", cfg.Space.Name, resp, time.Since(startTime).Seconds())
+	if status := gamma.CreateTable(ge.gamma, table); status.Code != 0 {
+		log.Error("create table [%s] err [%s] cost time: [%v]", cfg.Space.Name, status.Msg, time.Since(startTime).Seconds())
 		ge.Close()
-		return nil, fmt.Errorf("create gamma table has err:[%d]", resp)
+		return nil, fmt.Errorf("create gamma table has err:[%s]", status.Msg)
 	}
 
 	gammaDirs := make([]string, 0)
