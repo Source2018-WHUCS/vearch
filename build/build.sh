a873a7517444d50bb76e7e7868f8661bf96b6c1e
zhanchao1 <zhanchao3@jd.com>
2025-04-27 11:26:46 +0800

    refactor: update dockerfile... (#637)
    * refactor: update dockerfile
    
    * docs: bumping version to v3.5.7
    
    * chore: update build env faiss version to v1.10.0

--- build/build.sh
+++ build/build.sh
@@ -102,7 +102,7 @@ function build_thirdparty() {
   if [ ! -n "${FAISS_HOME}" ]; then
     if [[ ! -f "/usr/local/lib64/libfaiss.a" ]]; then
       wget -q https://github.com/facebookresearch/faiss/archive/refs/tags/v1.10.0.tar.gz
-      tar xf faiss-1.10.0.tar.gz
+      tar xf v1.10.0.tar.gz
       pushd faiss-1.10.0
       if [ -z $MKLROOT ]; then
         OS_NAME=$(uname)
@@ -110,16 +110,17 @@ function build_thirdparty() {
         if [ ${OS_NAME} == "Darwin" ]; then
           cmake -DFAISS_ENABLE_GPU=OFF -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_CXX_LIB_NAMES="libomp" -DOpenMP_libomp_LIBRARY="/usr/local/opt/libomp/lib" -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_OPT_LEVEL=avx2 -B build .
         elif [ ${ARCH} == "aarch64" -o ${ARCH} == "AARCH64" ]; then
-          cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$FAISS_HOME -B build .
+          cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -B build .
         else
-          cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_OPT_LEVEL=avx512 -D CMAKE_INSTALL_PREFIX=$FAISS_HOME -B build .
+          cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_OPT_LEVEL=avx512 -B build .
         fi
       else
-        cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_OPT_LEVEL=avx512 -D CMAKE_INSTALL_PREFIX=$FAISS_HOME -DBLA_VENDOR=Intel10_64_dyn -DMKL_LIBRARIES=$MKLROOT/lib/intel64 -B build .
+        cmake -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DFAISS_OPT_LEVEL=avx512 -DBLA_VENDOR=Intel10_64_dyn -DMKL_LIBRARIES=$MKLROOT/lib/intel64 -B build .
       fi
+
+      make -C build faiss && make -C build install
+      popd
     fi
-    make -C build faiss && make -C build install
-    popd
   fi
 
 }
